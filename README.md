e8ac834e10d864c59bac7612dad04c372a65170b
Joey Lin <pujoey@gmail.com>
2015-11-02 13:45:15 -0800

    Updated README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+![Train](http://www.funintofit.com/wp-content/uploads/2014/06/Morning-Workouts.jpg =404x283)
+# Train
+
+Description:  This app is for personal trainers
+to keep track of clients' sessions and for clients to
+keep track of their own progress
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+Session.delete_all
+Trainer.delete_all
+Message.delete_all
+
+
+garfield = User.create(
+  email:                 "bob.garfield@gmail.com",
+  handle:                "garfield minus morality",
+  profile_image_uri:     "https://goo.gl/RhIeZE",
+  # password:              "123",
+  # password_confirmation: "123"
+)
+
+book_list = [
+  {isbn: "9487123", title: "Catcher in the Rye",  author: "JD Salinger", edition: 2, has_read: true},
+  {isbn: "4328974", title: "Catch Me If You Can", author: "Someone",     edition: 1, has_read: false},
+  {isbn: "1239410", title: "Rye Rye Rye",         author: "Beth Samson", edition: 4, has_read: false},
+]
+
+Book.create book_list
